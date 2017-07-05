65edbe77fed696c12ce70bdba10cfba8a761a8f8
Kevin Chen <kevin.kang.chen@gmail.com>
2017-07-05 04:36:42 -0700

    feat(favorites): adds new favorites table

--- schema.sql
+++ schema.sql
@@ -6,17 +6,24 @@ USE flickr;
 
 CREATE TABLE images (
   id INT NOT NULL AUTO_INCREMENT,
-  flickr_id BIGINT,
+  flickr_id BIGINT unsigned,
   flickr_url VARCHAR(500),
   date_taken VARCHAR(30),
-  date_published VARCHAR(30),
+  date_published BIGINT unsigned,
   author VARCHAR(100),
   PRIMARY KEY(id)
 );
 
+CREATE TABLE favorites (
+  id INT NOT NULL AUTO_INCREMENT,
+  image_id BIGINT,
+  is_favorite TINYINT unsigned DEFAULT 0,
+  PRIMARY KEY(id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < schema.sql
  *  to create the database and the tables.*/
 
 -- INSERT INTO kanyes (era, year, description, imageUrl) VALUES ("College Dropout", 2004, "one of the all time best kanyes", "https://upload.wikimedia.org/wikipedia/en/a/a3/Kanyewest_collegedropout.jpg");
+-- INSERT INTO favorites (image_id, is_favorite) VALUES (35564982172, 1);
